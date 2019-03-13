4a3c83c589714772901038f4d90cf9a7e40db86d
Aeronica <pmboese@gmail.com>
2019-03-12 21:57:34 -0500

    Initial Commit

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,133 @@
+package com.example.examplemod;
+
+import com.example.examplemod.gui.GuiHandler;
+import com.example.examplemod.gui.GuiSimple;
+import de.johni0702.minecraft.gui.container.GuiScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.logging.log4j.Logger;
+
+@Mod.EventBusSubscriber
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "examplemod";
+    public static final String NAME = "Example Mod";
+    public static final String VERSION = "1.0";
+
+    public static final TheItem TEST_ITEM = new TheItem();
+
+    public static Logger logger;
+
+    @Mod.Instance(MODID)
+    public static ExampleMod instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        new GuiHandler(this).register();
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event)
+    {
+        logger.info("RegisterItems");
+        event.getRegistry().register(TEST_ITEM);
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, value = Side.CLIENT)
+    public static class ClientEventHandler
+    {
+        @SubscribeEvent
+        public static void registerModels(ModelRegistryEvent event)
+        {
+            logger.info("registerModels");
+            ModelLoader.setCustomModelResourceLocation(TEST_ITEM, 0, new ModelResourceLocation(TEST_ITEM.getRegistryName().toString(), "inventory"));
+        }
+
+        @SubscribeEvent
+        public static void event(PlayerInteractEvent.RightClickItem event)
+        {
+            if (event.getItemStack().getItem() instanceof TheItem)
+            {
+                openGui();
+            }
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    private static void openGui()
+    {
+        new GuiSimple(GuiScreen.wrap(Minecraft.getMinecraft().currentScreen), instance).display();
+    }
+
+    public static class TheItem extends Item
+    {
+
+        public TheItem()
+        {
+            setMaxStackSize(16);
+            setRegistryName("the_item");
+            setTranslationKey(getRegistryName().toString());
+            setCreativeTab(CreativeTabs.MISC);
+            hasSubtypes = false;
+        }
+
+        @Override
+        public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+        {
+            if (worldIn.isRemote)
+            {
+                logger.info("RightClicked");
+                return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+            }
+            return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+        }
+
+        @Override
+        public boolean isDamageable()
+        {
+            return false;
+        }
+
+        @Override
+        public boolean getShareTag()
+        {
+            return true;
+        }
+
+        @Override
+        public int getMaxItemUseDuration(ItemStack stack)
+        {
+            return 75;
+        }
+    }
+}
---
+++ src/main/java/com/example/examplemod/gui/GuiSimple.java
@@ -0,0 +1,109 @@
+package com.example.examplemod.gui;
+
+import com.example.examplemod.ExampleMod;
+import de.johni0702.minecraft.gui.container.GuiPanel;
+import de.johni0702.minecraft.gui.container.GuiScreen;
+import de.johni0702.minecraft.gui.element.GuiButton;
+import de.johni0702.minecraft.gui.element.GuiLabel;
+import de.johni0702.minecraft.gui.element.GuiTooltip;
+import de.johni0702.minecraft.gui.function.Closeable;
+import de.johni0702.minecraft.gui.layout.CustomLayout;
+import de.johni0702.minecraft.gui.layout.GridLayout;
+import de.johni0702.minecraft.gui.layout.HorizontalLayout;
+import de.johni0702.minecraft.gui.utils.Colors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class GuiSimple extends GuiScreen implements Closeable
+{
+    private final ExampleMod mod;
+
+    public GuiButton currentTabButton;
+    public GuiPanel currentTabPanel;
+
+    public final List<GuiPanel> tabPanels = new ArrayList<>();
+
+    public final GuiLabel warningLabel = new GuiLabel(this).setColor(Colors.RED)
+            .setI18nText("gui.examplemod.editor.disclaimer");
+
+    public final GuiPanel tabButtons = new GuiPanel(this).setLayout(new GridLayout().setSpacingX(5));
+
+    public final GuiButton backButton = new GuiButton().setI18nLabel("gui.examplemod.back").setSize(100, 20);
+
+    public final GuiPanel buttonPanel = new GuiPanel(this).setLayout(new HorizontalLayout().setSpacing(5))
+            .addElements(null, backButton);
+
+    public GuiSimple(GuiScreen parent, ExampleMod mod)
+    {
+        this.mod = mod;
+        backButton.onClick(parent::display);
+
+        setTitle(new GuiLabel().setI18nText("gui.examplemod.title"));
+        setBackground(Background.DIRT);
+
+        makeTab("import", GuiPanel::new).setDisabled(); // Not yet implemented
+        makeTab("upload", GuiPanel::new).setDisabled(); // Not yet implemented
+        makeTab("edit", GuiPanel::new).setDisabled(); // Not yet implemented
+
+
+        setLayout(new CustomLayout<GuiScreen>()
+        {
+            @Override
+            protected void layout(GuiScreen container, int width, int height)
+            {
+                // Move all inactive panels aside
+                tabPanels.forEach(e -> pos(e, Integer.MIN_VALUE, Integer.MIN_VALUE));
+
+                pos(warningLabel, 10, 22);
+                size(warningLabel, width - 20, 10);
+
+                pos(tabButtons, 10, y(warningLabel) + height(warningLabel) + 2);
+                size(tabButtons, width - 20, 20);
+
+                pos(buttonPanel, width - 10 - width(buttonPanel), height - 10 - height(buttonPanel));
+
+                if (currentTabPanel != null)
+                {
+                    pos(currentTabPanel, 10, y(tabButtons) + height(tabButtons) + 10);
+                    size(currentTabPanel, width - 20, y(buttonPanel) - 10 - y(currentTabPanel));
+                }
+            }
+        });
+    }
+
+    public GuiButton makeTab(String name, Supplier<GuiPanel> panel)
+    {
+        GuiButton button = new GuiButton().setI18nLabel("gui.examplemod.simple." + name + ".title")
+                .setTooltip(new GuiTooltip().setI18nText("gui.examplemod.simple." + name + ".description"))
+                .setSize(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        button.onClick(() ->
+                       {
+                           // Remove previous tab
+                           if (currentTabButton != null)
+                           {
+                               currentTabButton.setEnabled();
+                               removeElement(currentTabPanel);
+                           }
+
+                           // Activate new tab
+                           currentTabButton = button.setDisabled();
+                           addElements(null, currentTabPanel = panel.get());
+                       });
+        // Add new tab button to tabs panel
+        tabButtons.addElements(null, button);
+        ((GridLayout) tabButtons.getLayout()).setColumns(tabButtons.getChildren().size());
+        return button;
+    }
+
+    public ExampleMod getMod() {
+        return mod;
+    }
+
+    @Override
+    public void close()
+    {
+        mod.logger.info("Close SimpleGui");
+    }
+}
